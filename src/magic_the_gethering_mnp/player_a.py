a09be1681fe18a65e108028adb01055cadc4775a
Melangelo Guanzon <melangelo_guanzon@dlsu.edu.ph>
2026-08-05 23:53:00 +0800

    removed stuff from player a and b .py accidental push

--- src/magic_the_gethering_mnp/player_a.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import constants
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", action="store_true")
-args = parser.parse_args()
-
-constants.set_verbose(args.verbose)
